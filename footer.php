a06c90694af298edaf106aeaec8e6aad9909552a
Artem <artemlitvinov81@gmail.com>
2015-09-06 22:49:55 +0300

    go to begin again

---
+++ footer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the "site-content" div and all content after.
+ *
+ * @package WordPress
+ * @subpackage Rolli co
+ * @since Rolli co 1.0
+ */
+?>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
